4571e298817468924f416daa7515879fcfa7d3f4
DavidGoldman <dallasftball@gmail.com>
2014-10-12 02:15:58 -0400

    Fix server

--- BasicNodeTCPServer.js
+++ BasicNodeTCPServer.js
@@ -4,18 +4,19 @@ var net = require('net');
 // Creates a new TCP server. The handler argument is automatically set as a listener for the 'connection' event
 var server = net.createServer(function(socket) {
   console.log("Connection from " + socket.remoteAddress);
+  socket.setEncoding("ascii");
 
   socket.on("data", function(data) {
   	console.log("DATA " + socket.remoteAddress + ": " + data);
   });
 
-  socket.on("close", function(data) {
-  	console.log("CLOSED: " + socket.remoteAddress + " " + socket.remotePort);
+  socket.on("close", function(had_error) {
+  	console.log("CLOSED: " + had_error);
   });
 });
 
-// Fire up the server bound to port 7000 on localhost
-server.listen(7000, "localhost");
+// Fire up the server bound to port 80 on localhost
+server.listen(4000, "0.0.0.0");
 
 // Put a friendly message on the terminal
-console.log("TCP server listening on port 7000 at localhost.");
+console.log("TCP server listening on port 4000.");
